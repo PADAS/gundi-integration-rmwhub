0eb7f30e97dd169b9fddc15f6b33959972c47372
Chris Doehring <chrisdoehring@gmail.com>
2026-01-09 10:32:03 -0800

    fix: remove redundant coersion.

--- app/actions/rmwhub/types.py
+++ app/actions/rmwhub/types.py
@@ -49,19 +49,19 @@ class GearSet(BaseModel):
     when_updated_utc: str
 
     @validator("trawl_path", pre=True)
-    def none_to_empty(cls, v: object) -> object:
+    def none_to_empty(self, v: object) -> object:
         if v is None:
             return {}
         return v
 
     @validator("share_with", pre=True)
-    def none_to_empty_list(cls, v: object) -> object:
+    def none_to_empty_list(self, v: object) -> object:
         if v is None:
             return []
         return v
 
     @validator("deployment_type", pre=True)
-    def normalize_deployment_type(cls, v: object) -> str:
+    def normalize_deployment_type(self, v: object) -> str:
         """Normalize deployment_type to lowercase to handle API inconsistencies."""
         if v is None:
             return ""
